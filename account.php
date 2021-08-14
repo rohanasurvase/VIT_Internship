a35b6686d457cb9f2ea10aee8056ae06c611f177
shardul-b <shardulbirje15@gmail.com>
2021-08-15 00:58:55 +0530

    Account

--- account.php
+++ account.php
@@ -26,10 +26,24 @@
         $selectDetails="SELECT * FROM `user_info` WHERE `user_id`='". $userID ."'";
         $queryResult = mysqli_query($connection, $selectDetails);
         $userDetails = mysqli_fetch_assoc($queryResult);
-        $selectStudentDetails="SELECT `batch` FROM `student` WHERE `student_id`='". $userID ."'";
-        $studentQueryResult = mysqli_query($connection, $selectStudentDetails);
-        $studentDetails = mysqli_fetch_assoc($studentQueryResult);
+        $studentDetails='';
+        if($userDetails['type']==="student"){
+            $selectStudentDetails="SELECT * FROM `student` WHERE `student_id`='". $userID ."'";
+            $studentQueryResult = mysqli_query($connection, $selectStudentDetails);
+            $studentDetails = mysqli_fetch_assoc($studentQueryResult);
+        }
+        
+        
+        
+                                
         // echo($userDetails);
+    ?>
+    <?php 
+        if (!is_null($studentDetails['group_id'])) {
+            // echo("Yo");
+            $selectGroupDetails="SELECT * FROM `group_details` WHERE `group_id`='". $studentDetails['group_id'] ."'";    
+            $selectProjectDetails="SELECT `project_id`,`project_name`,`project_desc`,`guideid`,`technologies` FROM `project` WHERE `group_id`='". $studentDetails['group_id'] ."'";
+        }
     ?>
     <!-- Profile details update -->
     <div class="modal fade" id="editProfileModal" tabindex="-1" role="dialog" aria-labelledby="editProfileModalLabel" aria-hidden="true">
@@ -80,9 +94,9 @@
                         <?php
                             $batchVal='';
                             if(isset($studentDetails["batch"])){
-                                $bacthVal=$studentDetails["batch"];
+                                $batchVal=$studentDetails["batch"];
                             }else{
-                                $bacthVal="";
+                                $batchVal="";
                             }
                             if($userDetails["type"]==="student"){
                                 echo'<div class="form-group">
@@ -92,22 +106,8 @@
                                     </div>';
                             }
                         ?>
-                        <!-- <div class="form-group">
-                            <label for="profile-location" class="col-form-label">Location:</label>
-                            <input type="text" class="form-control" id="profile-location">
-
-                        </div> -->
-                        <!-- <div class="row">
-                            <div class="col">
-                                <label for="branch">Branch</label>
-                                <input type="text" class="form-control" placeholder="Branch" id="branch">
-                            </div>
-                            <div class="col">
-                                <label for="pass-year">Passing Year</label>
-                                <input type="number" class="form-control" placeholder="Passing Year" id='pass-year'>
-                            </div>
-                        </div> -->
-                        <button type="button" type="submit" class="btn btn-primary" name="details-submit">Update Details</button>
+                        
+                        <button type="submit" class="btn btn-primary" name="details-submit">Update Details</button>
                     </form>
                 </div>
               <div class="modal-footer">
@@ -129,29 +129,51 @@
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
+          <form method="POST" action="">
           <div class="modal-body">
-            <form>
+            
                 <div class="form-group">
                     <label for="project-name" class="col-form-label">Project Name</label>
-                    <input type="text" class="form-control" id="project-name">
+                    <input type="text" class="form-control" id="project-name" name="project-name">
+                </div>
+                <div class="form-group">
+                    <label for="project-link" class="col-form-label">Project link</label>
+                    <input type="text" class="form-control" id="project-link" name="project-link">
+                </div>
+                <div class="form-group">
+                    <label for="project-technologies" class="col-form-label">Techonologies used</label>
+                    <input type="text" class="form-control" id="project-technologies" placeholder="eg: HTML, CSS" data-toggle="tooltip" data-placement="right" title="Use commas for entering multiple Techonologies/languages" name='skills'>
+                    <!-- <button class="btn btn-secondary" id='skills-submit'>Add</button> -->
+                    <!-- <p class="skills" id='skills-box'> -->
+                        
+                    </p>
                 </div>
                 <div class="form-group">
                     <label for="project-guide" class="col-form-label">Guide Name</label>
-                    <input type="text" class="form-control" id="project-guide">
+                    <input type="text" class="form-control" id="project-guide" name="project-guide" placeholder='Enter guide code' data-toggle="tooltip" data-placement="right" title="For eg:Enter RS for Rohana Survase" value="<?php  
+                        if(isset($studentDetails['guide_id'])){
+                            $GuideName="SELECT `guide_code` FROM `guide` where `guide_id`='". $studentDetails['guide_id'] ."'";
+                            $result = mysqli_query($connection, $GuideName);
+                            if (mysqli_num_rows($result) > 0) {
+                                $row = mysqli_fetch_assoc($result);
+                                echo $row['guide_code'];
+                            }
+                        }
+                    ?>">
                 </div>
                 <div class="form-group">
                     <label for="member-count" class="col-form-label">Group Member count</label>
-                    <input type="text" class="form-control" id="member-count" placeholder="Member count (excluding you)">
+                    <input type="number" class="form-control" id="member-count" name="member-count" placeholder="Member count (excluding you)">
                     <p id="error-text"></p>
                 </div>
                 <div class="row">
                     <div class="col member-input-container hide">
-                        <label for="member-1">Member Name</label>
-                        <input type="text" class=" form-control" placeholder="Member 1 Name" id="member-1">
+                        <label for="member-1">Member Email</label>
+                        <input type="email" class=" form-control" placeholder="Member 1 Email" id="member-1" name="member-1">
                     </div>
                     <div class="col member-input-container hide">
-                        <label for="member-2">Member Name</label>
-                        <input type="text" class="form-control" placeholder="Member 2 Name" id='member-2'>
+                        <label for="member-2">Member Email</label>
+                        <input type="email" class="form-control" placeholder="Member 2 Email" id='member-2' name="member-2">
                     </div>
                 </div>
                 <!-- <div class="form-group member-input-container">
@@ -160,16 +182,18 @@
                 </div> -->
                 <div class="form-group">
                     <label for="project-desc" class="col-form-label">Project Description</label>
-                    <textarea class="form-control" id="project-desc"></textarea>
+                    <textarea class="form-control" id="project-desc" name="project-desc"></textarea>
                 </div>
                 
                 
-            </form>
+            
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Update Details</button>
+            <button type="submit" name='project-submit' class="btn btn-primary">Update Details</button>
+            
           </div>
+          </form>  
         </div>
       </div>
     </div>
@@ -200,7 +224,7 @@
                                         if($studentDetails["batch"]==0){
                                             echo "<p>Enter Batch</p>";
                                         }else{
-                                            echo "<p>Batch of </p>".$studentDetails['batch'];
+                                            echo "<p>Batch of ".$studentDetails['batch']."</p>";
                                         }
                                     }
                                  ?>
@@ -229,19 +253,27 @@
                         </div>
                         <div class="container">
                             <?php
+
+                                /*$selectProject="SELECT `project_id`,`project_name`,`project_desc`,`guideid`,`technologies` FROM `project` WHERE `group_id`='".:
+                                $selectProject='';
+                                if($userDetails['type']==="student"){
+                                    $selectProject="SELECT"
+                                }else{
+
+                                }
                                 $check="";                            
-                                $selectID="SELECT `group_id` FROM `student` WHERE `student_id`='" .$userID. "'";
+                                $selectProject="SELECT `group_id` FROM `student` WHERE `student_id`='" .$userID. "'";
                                 $selectResult = mysqli_query($connection, $selectID);
                                 if(mysqli_num_rows($selectResult) > 0){
-                                    $details = mysqli_fetch_assoc($result);
+                                    $details = mysqli_fetch_assoc($selectResult);
                                     $check=is_null($details['group_id']);
                                 }
                                 else{
                                     //Do not Display any projects
                                     // echo "No Rows Found";
-                                }
+                                }*/
                                 // Type=student
-                                if($_SESSION["user_id"]===$userID){
+                                if($_SESSION["user_id"]===$userID && $userDetails['type']==="student"){
                                     echo 
                                     '<button class="btn btn-outline-secondary my-2" data-toggle="modal" data-target="#addProjectModal">
                                         
@@ -338,6 +370,14 @@
 
             }
         });
+        /*let skillsReference=document.getElementById('skills-submit');
+        skills.addEventListener('click',()=>{
+            let skills=document.getElementById('project-technologies').value;
+            let arr=skills.split(',');
+            for(let i of arr){
+                document.getElementById('skills-box').innerHTML+=i;
+            }
+        });*/ 
     </script>
     <?php
     if(isset($_POST['logout'])){
@@ -379,7 +419,144 @@
         }
         // $studentQuery="UPDATE `student` SET ``"
     }else{
-        echo "Button Error";
+        // echo "Button Error";
+    }
+?>
+<!-- Project -->
+<?php 
+    if (isset($_POST['project-submit'])) {
+        $projectName=$_POST['project-name'];
+        $projectLink=$_POST['project-link'];
+        $projectDesc=$_POST['project-desc'];
+        $projectGuide=$_POST['project-guide'];
+        $technologies=$_POST['skills'];
+        $technologies=explode($technologies,',');
+        $count=$_POST['member-count'];
+        $member2='';
+        $memberCheck='';
+        $member1=$_POST['member-1'];
+        // Select Guide Id
+        $guideSelect="SELECT `guide_id` FROM `guide` WHERE `guide_code`='".$projectGuide."'";
+        $guideResult = mysqli_query($connection, $guideSelect);
+        $guideRow='';
+        $value=[];
+        if(mysqli_num_rows($guideResult) > 0){
+            $guideRow = mysqli_fetch_assoc($guideResult);
+            //echo $count;
+            
+            //print_r($guideRow);
+            // Only One Member
+            if($count==1){
+                $memberCheck="SELECT `user_id` FROM `user_info` WHERE `email`='". $member1 ."'";
+                $memberResult = mysqli_query($connection, $memberCheck);
+                if(mysqli_num_rows($memberResult) > 0){
+                     $memberRow = mysqli_fetch_array($memberResult, MYSQLI_NUM);
+                    //$memberRow = mysqli_fetch_assoc($memberResult);
+                    array_push($value,$memberRow[0]);
+                    //$value=$memberRow;
+                    // $updateStudent="UPDATE TABLE `student` SET `guide_id`=";
+                }else{
+                    echo"Member Account doesn't exist";
+                }
+            }
+            // Two members
+            else{
+                $member2=$_POST['member-2'];
+                $memberCheck1="SELECT `user_id` FROM `user_info` WHERE `email` IN ('".$member1."','".$member2."')";
+                $memberResult1 = mysqli_query($connection, $memberCheck1);
+                if(mysqli_num_rows($memberResult1)==1){
+
+                    $memberRow1 = mysqli_fetch_array($memberResult1, MYSQLI_NUM);
+                    //$memberRow1 = mysqli_fetch_assoc($memberResult1);
+                    if(in_array($member1, $memberRow1)){
+                        echo $member2." doesn't exist";
+                    }else{
+                        echo $member1." doesn't exist";
+                    }
+                    //$memberRow1 = mysqli_fetch_assoc($memberResult1);
+                    // $updateStudent="UPDATE TABLE `student` SET `guide_id`=";
+                }else if (mysqli_num_rows($memberResult1)==2) {
+                    //$memberRow1 = mysqli_fetch_assoc($memberResult1);
+                    $memberRow1 = mysqli_fetch_array($memberResult1, MYSQLI_NUM);
+                    for ($i=0; $i<2; $i++) { 
+                        array_push($value,$memberRow1[$i]);
+                    }
+                    
+                    //$value=;
+                }
+                else{
+                    echo"Member Account doesn't exist";
+                }
+            }
+            
+        }
+        // Guide account doesn't exist
+        else{
+            echo"Guide Account Doesn't Exist";  
+        }
+        array_push($value,$userID);
+        // print_r($value);
+        //print_r(array_count_values($value));
+        // $group_id='';
+        $group_id=$value[0].''.$value[1].''.$guideRow['guide_id'];
+
+        $group_id=str_replace('s', '', $group_id);
+        $updateStudent='';
+        $group_member_ids='';
+        $select_usernames='';
+        $selectRow='';
+        if(count($value)==2){
+            $group_member_ids=$value[0].','.$value[1];
+            $select_usernames="SELECT `username` from `user_info` WHERE `user_id` IN('". $value[0] ."','". $value[1] ."')";
+            $selectResult=mysqli_query($connection, $select_usernames);
+            if (mysqli_num_rows($selectResult) > 0) {
+                $selectRow = mysqli_fetch_array($selectResult,MYSQLI_NUM);
+            }
+            $group_member_names=$selectRow[0].','.$selectRow[1];
+            $updateStudent="UPDATE `student` SET `group_id`='". $group_id ."',`guide_id`='". $guideRow['guide_id'] ."' WHERE `student_id` IN ('". $value[0] ."','". $value[1] ."')";
+
+        }else{
+            if(isset($value[2])){
+                $group_member_ids=$value[0].','.$value[1].','.$value[2];
+                $select_usernames="SELECT `username` from `user_info` WHERE `user_id` IN('". $value[0] ."','". $value[1] ."','". $value[2] ."')";
+                $selectResult=mysqli_query($connection, $select_usernames);
+                if (mysqli_num_rows($selectResult) > 0) {
+                    $selectRow = mysqli_fetch_array($selectResult,MYSQLI_NUM);
+                }
+                $group_member_names=$selectRow[0].','.$selectRow[1].','.$selectRow[2];
+                $updateStudent="UPDATE `student` SET `group_id`='". $group_id ."',`guide_id`='". $guideRow['guide_id'] ."' WHERE `student_id` IN('". $value[0] ."','". $value[1] ."','". $value[2] ."')";
+            }
+        }
+        
+
+        if (mysqli_query($connection, $updateStudent)) {
+            
+        }else {
+            echo "Error updating record: " . mysqli_error($connection);
+        } 
+        $ProjectChecker="SELECT  `project_id` FROM `project` WHERE `group_id`='". $group_id ."'";
+        $ProjectCheckerresult = mysqli_query($connection, $ProjectChecker);        
+        //Update in morning (INSERT and UPDATE to PROJECT)
+        if(!mysqli_num_rows($ProjectCheckerresult) > 0){
+            $projectInsert="INSERT INTO `project`( `project_name`, `project_desc`, `guideid`, `group_id`, `project_link`, `technologies`) VALUES ('". $project_name ."',[value-6],[value-7],[value-9],[value-10],[value-11])";
+        }else{
+            $row = mysqli_fetch_assoc($ProjectCheckerresult);
+            $projectUpdate="UPDATE TABLE `project` SET `project_name`, `project_desc`, `guideid`, `group_id`, `project_link`, `technologies` WHERE project_id='". $row['project_id'] ."'";
+            
+        }
+        // Verify if group already exists.
+        $GroupChecker="SELECT  `group_id` FROM `group_details` WHERE `project_id`='". $row['project_id'] ."'";
+        $GroupCheckerresult = mysqli_query($connection, $GroupChecker);
+        $insertGroup="INSERT INTO `group_details`(`group_id`, `group_member_count`, `group_member_ids`, `group_member_names`, `guide_id`, `project_id`) VALUES ('". $group_id ."','". $count+1 ."','". $group_member_ids ."','". $group_member_names ."','". $guideRow['guide_id'] ."',[value-6])";
+        /*
+        if(is_null($studentDetails["group_id"])){
+            
+            $groupId=
+            $insertGroup="INSERT INTO `group_details`(`group_id`, `group_member_count`, `group_member_ids`, `group_member_names`, `guide_id`, `project_id`) VALUES ()";    
+        }*/
+        
+        
+        // $studentQuery="UPDATE `student` SET ``"
     }
 ?>
   
